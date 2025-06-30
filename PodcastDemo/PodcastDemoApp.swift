82bdfd9d1444a065d970f909bfb4a9d4f323d862
nF <naalmuaither@elm.sa>
2025-06-30 20:00:48 +0300

    Initial Commit

---
+++ PodcastDemo/PodcastDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastDemoApp.swift
+//  PodcastDemo
+//
+//  Created by Nawaf  on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PodcastDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
