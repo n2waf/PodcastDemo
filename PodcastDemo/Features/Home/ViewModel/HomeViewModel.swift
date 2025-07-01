969c300fc81afc33917717ef6e367a61a25148ea
nF <naalmuaither@elm.sa>
2025-07-01 22:30:20 +0300

    Sorts home sections to prioritize queue

--- PodcastDemo/Features/Home/ViewModel/HomeViewModel.swift
+++ PodcastDemo/Features/Home/ViewModel/HomeViewModel.swift
@@ -19,9 +19,20 @@ class HomeViewModel: ObservableObject {
         
         do {
             let response = try await networkManager.fetch(HomeResponse.self, from: url)
-            sections = response.sections
+            sections = sortSectionsWithQueueFirst(response.sections)
         } catch {
             print("Error loading sections: \(error)")
         }
+    }
+    
+    private func sortSectionsWithQueueFirst(_ sections: [HomeSection]) -> [HomeSection] {
+        let queueSections = sections.filter { $0.info.displayType == .queue }
+        let otherSections = sections.filter { $0.info.displayType != .queue }
+        
+        let sortedQueueSections = queueSections.sorted { $0.info.order < $1.info.order }
+        let sortedOtherSections = otherSections.sorted { $0.info.order < $1.info.order }
+        
+        
+        return sortedQueueSections + sortedOtherSections
     }
 }
